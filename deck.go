dc7b0284b2659e8d3c52c5fc6f140073bc8942c8
kacmacuna <bkurtanidze11@gmail.com>
2020-10-04 21:40:37 +0400

    first commit

---
+++ deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			cards = append(cards, cardSuit+" of "+cardValue)
+		}
+	}
+
+	return cards
+
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
